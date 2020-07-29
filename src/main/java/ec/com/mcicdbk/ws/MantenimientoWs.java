994aa4b79432b3038ba087a20f243769c06d787a
Jonnathan Juma <jonnathanjuma@gmail.com>
2020-07-29 16:20:27 -0500

    sol java

--- src/main/java/ec/com/mcicdbk/ws/MantenimientoWs.java
+++ src/main/java/ec/com/mcicdbk/ws/MantenimientoWs.java
@@ -1,5 +1,6 @@
 package ec.com.mcicdbk.ws;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MantenimientoWs {
 
+	@GetMapping("/prueba")
 	public String prueba () {
 		return "funciona";
 	}
